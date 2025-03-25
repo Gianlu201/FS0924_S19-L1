0677c04a64ab1e9db891066527e5aee7e33250ce
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-03-25 15:55:54 +0100

    Cors added

--- FS0924_S19-L1/Program.cs
+++ FS0924_S19-L1/Program.cs
@@ -20,6 +20,8 @@ builder.Services.AddScoped<StudentService>();
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
